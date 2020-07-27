32dd8cc072ab46fa69f4cb22a5dbe2a60ca23d82
Delete020 <delete020@protonmail.com>
2020-07-27 11:50:06 +0800

    hw0 completed

--- HW0/DrawTriangle.java
+++ HW0/DrawTriangle.java
@@ -1,15 +1,14 @@
-package HW0;
-
 public class DrawTriangle {
-    public static void DrawTriangle(int n){
-        int j=0;
-        while (j<n) {
+    public static void DrawTriangle(int n) {
+        int j = 0;
+        while (j < n) {
             for (int i = 0; i < j; i++) {
                 System.out.println("*");
             }
             j++;
         }
     }
+
     public static void main(String[] args) {
         DrawTriangle(10);
     }
--- HW0/MaxNumber.java
+++ HW0/MaxNumber.java
@@ -1,21 +1,18 @@
-package HW0;
-
 public class MaxNumber {
-    public static int max(int[] m){
-        int max=0;
-        for (int i = 0; i<m.length;i++){
-            if(m[i]>max){
-                max=m[i];
+    public static int max(int[] m) {
+        int max = 0;
+        for (int i : m) {
+            if (i > max) {
+                max = i;
             }
         }
         System.out.println(max);
         return max;
     }
+
     public static void main(String[] args) {
         int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
-        //max(numbers);
-        MaxNumber m = new MaxNumber();
-        m.max(numbers);
+        max(numbers);
     }
 
 }
